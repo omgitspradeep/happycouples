207ec88f5427993dda20202b530ab5dd123344a0
omgitspradeep <63057831+omgitspradeep@users.noreply.github.com>
2021-02-14 21:05:29 +0545

    Update urls.py

--- happycouples/urls.py
+++ happycouples/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 from django.conf.urls import url
 from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
