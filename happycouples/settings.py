26d29f0a9bfbbc274612dd52342f3ff30e836864
omgitspradeep <63057831+omgitspradeep@users.noreply.github.com>
2021-02-14 20:50:52 +0545

    Update settings.py

--- happycouples/settings.py
+++ happycouples/settings.py
@@ -124,8 +124,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_DIRS = [ STATIC_DIR, ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 SESSION_COOKIE_NAME ='guestsession'
