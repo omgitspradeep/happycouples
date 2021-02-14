3a9fe45a1a468353ea0bc6b66444a84242856146
omgitspradeep <63057831+omgitspradeep@users.noreply.github.com>
2021-02-14 20:24:20 +0545

    Update settings.py

--- happycouples/settings.py
+++ happycouples/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ptafd*ujg1*5cngo4%0e27=nlgrtk53-94%w=zp=za57f(%md7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'mydomain.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'happycouples.herokuapp.com']
 
 
 # Application definition
